9fcf7228905f8f2e9a42b2eca5eec06584163c44
jiangjinteng <jiangjinteng@126.com>
2017-03-26 10:06:57 +0800

    中文支持

---
+++ cn.ts
@@ -0,0 +1,260 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en">
+<context>
+    <name>GaussianBlurDialog</name>
+    <message>
+        <location filename="gaussianblurdialog.ui" line="20"/>
+        <source>Dialog</source>
+        <translation>高斯模糊</translation>
+    </message>
+    <message>
+        <location filename="gaussianblurdialog.ui" line="28"/>
+        <source>Input value for radius:</source>
+        <translation>请输入模糊程度：</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>ImageQt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="31"/>
+        <source>Open</source>
+        <translation>打开</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="38"/>
+        <location filename="mainwindow.ui" line="186"/>
+        <source>Close</source>
+        <translation>清除</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="45"/>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="56"/>
+        <source>+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <location filename="mainwindow.ui" line="245"/>
+        <source>Restore</source>
+        <translation>恢复</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="70"/>
+        <source>-</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="103"/>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="114"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="118"/>
+        <source>Add Frame</source>
+        <translation>添加相框</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="126"/>
+        <source>Colour temperature</source>
+        <translation>色温调节</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="133"/>
+        <source>Add texture</source>
+        <translation>添加纹理</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="147"/>
+        <source>Tools</source>
+        <translation>工具</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="151"/>
+        <source>Rotate</source>
+        <translation>旋转</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="162"/>
+        <source>Help</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="178"/>
+        <source>Open...</source>
+        <translation>打开...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="181"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="195"/>
+        <source>Save...</source>
+        <translation>保存...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="198"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="203"/>
+        <source>Save As...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="208"/>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="211"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="220"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="225"/>
+        <source>Zoom In</source>
+        <translation>放大</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="230"/>
+        <source>Zoom Out</source>
+        <translation>缩小</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="235"/>
+        <source>Normal</source>
+        <translation>原始尺寸</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="240"/>
+        <source>Adjust</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="250"/>
+        <source>Left</source>
+        <translation>左旋转</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="255"/>
+        <source>Right</source>
+        <translation>右旋转</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="260"/>
+        <source>Zoom</source>
+        <translation>缩放</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="265"/>
+        <source>Gaussian blur</source>
+        <translation>高斯模糊</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="270"/>
+        <source>Grayscale</source>
+        <translation>灰度图像</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="275"/>
+        <source>Warm</source>
+        <translation>暖色调</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="280"/>
+        <source>Cool</source>
+        <translation>冷色调</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="285"/>
+        <source>Movie frame</source>
+        <translation>电影</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="290"/>
+        <source>Classic frame</source>
+        <translation>经典</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="295"/>
+        <source>Flower frame</source>
+        <translation>小花</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="300"/>
+        <source>Metal</source>
+        <translation>金属</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="305"/>
+        <source>Adjust brightness</source>
+        <translation>调整亮度</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="261"/>
+        <source>Open image</source>
+        <translation>打开图片</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="262"/>
+        <source>All Files (*);;All Images (*.bpm *.gif *.jpg *.jpeg *.png *.ppm *.xbm *.xpm);;Image BPM (*.bpm);;Image GIF (*.gif);;Image JPG (*.jpg);;Image JPEG (*.jpeg);;Image PNG (*.png);;Image PPM (*.ppm);;Image XBM (*.xbm);;Image XPM (*.xpm);;</source>
+        <translation>所有文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="279"/>
+        <source>Unable to open image.</source>
+        <translation>无法打开文件.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="321"/>
+        <source>Save image</source>
+        <translation>保存图像</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="322"/>
+        <source>All files (*);;Image BPM (*.bpm);;Image GIF (*.gif);;Image JPG (*.jpg);;Image JPEG (*.jpeg);;Image PNG (*.png);;Image PPM (*.ppm);;Image XBM (*.xbm);;Image XPM (*.xpm);;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="336"/>
+        <source>Unable to save image.</source>
+        <translation>无法保存图像.</translation>
+    </message>
+</context>
+<context>
+    <name>ZoomDialog</name>
+    <message>
+        <location filename="zoomdialog.ui" line="20"/>
+        <source>图像缩放 - ImageQt</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="zoomdialog.ui" line="41"/>
+        <source>请输入缩放倍率(%)：</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
