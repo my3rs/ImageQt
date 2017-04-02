20fa9f900b25c8a5df3247bf71b1a0e45c9d81e0
jiangjinteng <jingjinteng@126.com>
2017-04-02 10:49:52 +0800

    新增“绘制图像直方图”

---
+++ histogram.cpp
@@ -0,0 +1,6 @@
+#include "histogram.h"
+
+Histogram::Histogram()
+{
+
+}
---
+++ histogram.h
@@ -0,0 +1,11 @@
+#ifndef HISTOGRAM_H
+#define HISTOGRAM_H
+
+
+class Histogram : public QLabel
+{
+public:
+    Histogram();
+};
+
+#endif // HISTOGRAM_H
