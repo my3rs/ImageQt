a6f3edffc82e34440ef81e57d8cdc08c0a05ed3c
jiangjinteng <jingjinteng@126.com>
2017-04-03 20:59:27 +0800

    新增“灰度指数变换”

--- dialog_exp_transform.cpp
+++ dialog_exp_transform.cpp
@@ -6,9 +6,52 @@ DialogExpTransform::DialogExpTransform(QWidget *parent) :
     ui(new Ui::DialogExpTransform)
 {
     ui->setupUi(this);
+
+    paintFunctionImage(ui->bDoubleSpinBox->value(), ui->cDoubleSpinBox->value(), ui->aDoubleSpinBox->value());
 }
 
 DialogExpTransform::~DialogExpTransform()
 {
     delete ui;
+}
+
+void DialogExpTransform::on_buttonBox_accepted()
+{
+    emit sendData(ui->bDoubleSpinBox->value(), ui->cDoubleSpinBox->value(), ui->aDoubleSpinBox->value());
+}
+
+void DialogExpTransform::paintFunctionImage(double b, double c, double a)
+{
+    // generate some data:
+    QVector<double> x(1001), y(1001); // initialize with entries 0..100
+    for (int i=0; i<1001; ++i)
+    {
+      x[i] = i/50.0 - 10;
+      y[i] = qPow(b, c*(x[i]-a));
+    }
+    // create graph and assign data to it:
+    ui->customPlot->addGraph();
+    ui->customPlot->graph(0)->setData(x, y);
+    // give the axes some labels:
+    ui->customPlot->xAxis->setLabel("x");
+    ui->customPlot->yAxis->setLabel("y");
+    // set axes ranges, so we see all data:
+    ui->customPlot->xAxis->setRange(-10, 10);
+    ui->customPlot->yAxis->setRange(-10, 10);
+    ui->customPlot->replot();
+}
+
+void DialogExpTransform::on_bDoubleSpinBox_valueChanged(double arg1)
+{
+    paintFunctionImage(arg1, ui->cDoubleSpinBox->value(), ui->aDoubleSpinBox->value());
+}
+
+void DialogExpTransform::on_cDoubleSpinBox_valueChanged(double arg1)
+{
+    paintFunctionImage(ui->bDoubleSpinBox->value(), arg1, ui->aDoubleSpinBox->value());
+}
+
+void DialogExpTransform::on_aDoubleSpinBox_valueChanged(double arg1)
+{
+    paintFunctionImage(ui->bDoubleSpinBox->value(), ui->cDoubleSpinBox->value(), arg1);
 }
--- dialog_exp_transform.h
+++ dialog_exp_transform.h
@@ -17,6 +17,17 @@ public:
 
 private:
     Ui::DialogExpTransform *ui;
+
+    void paintFunctionImage(double b, double c, double a);
+
+signals:
+    void sendData(double, double, double);
+
+private slots:
+    void on_buttonBox_accepted();
+    void on_bDoubleSpinBox_valueChanged(double arg1);
+    void on_cDoubleSpinBox_valueChanged(double arg1);
+    void on_aDoubleSpinBox_valueChanged(double arg1);
 };
 
 #endif // DIALOG_EXP_TRANSFORM_H
